cd31e0d83309c9585225b7070b062a298139af6a
Matteo Peixoto <matteopeixotoo@gmail.com>
2026-02-25 17:21:51 -0300

    Rename encrypt function to caesar

--- cypher_of_caesar.py
+++ cypher_of_caesar.py
@@ -1,15 +1,13 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt(original_text, shift_amount, direction_message):
+def caesar(original_text, shift_amount, direction_message):
     code_text = ""
     if direction_message == 'encode':
         for char in original_text:
             if char in alphabet:
-                move_original_text = alphabet.index(char) + shift_amount
-                if move_original_text > 25:
-                    move_original_text = alphabet.index("a") - (25 - alphabet.index(char)) + (shift_amount - 1)
+                move_original_text = (alphabet.index(char) + shift_amount) % 26
                 code_text += alphabet[move_original_text]
-            elif char == " ":
+            elif char == ' ':
                 code_text += " "
             else:
                 code_text += char
@@ -17,11 +15,9 @@ def encrypt(original_text, shift_amount, direction_message):
     elif direction_message == 'decode':
         for char in original_text:
             if char in alphabet:
-                move_original_text = alphabet.index(char) - shift_amount
-                if move_original_text < 0:
-                    move_original_text = alphabet.index("z") + (alphabet.index(char)) - (shift_amount - 1)
+                move_original_text = (alphabet.index(char) - shift_amount) % 26
                 code_text += alphabet[move_original_text]
-            elif char == " ":
+            elif char == ' ':
                 code_text += " "
             else:
                 code_text += char
@@ -33,12 +29,11 @@ while final != "no":
     if direction == "encode" or direction == "decode":
         text = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
-        encrypt(text, shift, direction)
+        caesar(text, shift, direction)
         final = input("Type 'yes' to continue or 'no' to break: \n").lower()
     else:
         direction = input("Invalide Value! Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
         text = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
-        encrypt(text, shift, direction)
+        caesar(text, shift, direction)
         final = input("Type 'yes' to continue or 'no' to break: \n").lower()
-      
